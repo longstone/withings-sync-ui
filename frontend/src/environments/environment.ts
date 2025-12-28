64d5695c9a0292f8a11362280475ea71581fd6f0
longstone <2110765+longstone@users.noreply.github.com>
2025-12-29 00:05:00 +0100

    fix: env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -11,6 +11,6 @@ const runtimeEnv = typeof window !== 'undefined' ? window.__env : undefined
 
 export const environment = {
   production: false,
-  apiUrl: runtimeEnv?.apiUrl ?? 'http://localhost:3333/api',
+  apiUrl: runtimeEnv?.apiUrl ?? '/api',
   wsUrl: runtimeEnv?.wsUrl ?? 'ws://localhost:3333'
 }
